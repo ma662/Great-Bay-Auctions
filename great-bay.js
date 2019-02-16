6e26606faf76c99f265ef157abf7ae8fc126f1e4
Chlora <ma662@njit.edu>
2019-02-15 19:10:50 -0500

    major functionality

--- great-bay.js
+++ great-bay.js
@@ -16,74 +16,91 @@ var options = {
     firstRun : true,
 
     chooseOption : function() {
+        if (user.authenticated) {
 
-        var msg = "";
-        if (options.firstRun){
-            msg = "Welcome to Great-Bay Auctions!";
+            var msg = "";
+            if (options.firstRun){
+                msg = "Welcome to Great-Bay Auctions, " + user.username +" !";
+            }
+            else {
+                msg = "Would you like to do something else?";
+            }
+
+            inquirer
+            .prompt([
+                {
+                    name: "option",
+                    type: "list",
+                    message: msg,
+                    choices: ["[ bid on an item ]", "[ post an item ]", "[ display items ]", "[ logout ]", "[ exit ]"]
+                }
+            ])
+            .then(answers => {
+                var sel = answers.option.split(' ');
+
+                if (sel != "exit") {
+                    sel = sel[1];
+                }
+        
+                switch (sel) {
+                    case 'bid' : 
+                        // console.log("Bid Case Running ... ");
+                        options.firstRun = false;
+                        options.bidFunc();
+                    break;
+        
+                    case 'post' :
+                    // console.log("Post Case Running ... ");
+                        options.firstRun = false;
+                        options.postFunc();
+                    // post logic
+                    break;
+
+                    case 'display' :
+                    // console.log("Display Case Running ... ");
+                        options.firstRun = false;
+                        options.dispList(true);
+                    break;
+
+                    case "logout":
+                        user.logout();
+                    break;
+
+                    case 'exit' :
+                    // exit logic
+                        options.quit();
+                    break;
+        
+                    default :
+                        console.log("default case stuff");
+                    break;
+                }
+            });
         }
         else {
-            msg = "Would you like to do something else?";
+            console.log("Please sign-in to use these features.");
+            home();
         }
-
-        inquirer
-        .prompt([
-            {
-                name: "option",
-                type: "list",
-                message: msg,
-                choices: ["BID ON AN ITEM", "POST AN ITEM", "DISPLAY ITEMS", "EXIT"]
-            }
-        ])
-        .then(answers => {
-            var sel = answers.option.split(' ')[0];
-    
-            switch (sel) {
-                case 'BID' : 
-                    console.log("Bid Case Running ... ");
-                    options.firstRun = false;
-                    options.bidFunc();
-                break;
-    
-                case 'POST' :
-                console.log("Post Case Running ... ");
-                    options.firstRun = false;
-                    options.postFunc();
-                // post logic
-                break;
-
-                case 'DISPLAY' :
-                console.log("Display Case Running ... ");
-                    options.firstRun = false;
-                    options.dispList(true);
-                break;
-
-                case 'EXIT' :
-                // exit logic
-                    console.log("Ok, Goodbye!");
-                    connection.end();
-                    process.exit();
-                break;
-    
-                default :
-                    console.log("default case stuff");
-                break;
-            }
-        });
     },
 
     bidFunc : function()
     {
-        connection.query("SELECT * FROM all_stuff", function(err, res) {
+        connection.query("SELECT * FROM auction_items", function(err, res) {
             if (err) throw (err);
 
             var choicesArr = [];
             for (var i=0; i<res.length; i++){
-                var choicesObj = {
-                        name: ((i+1) + " - [" + res[i].thingy_type + "] -- " + res[i].title) + " || Highest Bid: " + res[i].current_bid,
-                        value: res[i].id,
-                        short: res[i].thingy_type
-                    }
+                var formatted = parseFloat(Math.round(res[i].current_bid * 100) / 100).toFixed(2);
 
+                var myStr = ( i + 1) + ") [" + res[i].thingy_type + "] [poster: " + res[i].poster + "] -- " + res[i].title + " || Highest Bid: " + formatted + " (" + res[i].current_highest_bidder + ")";
+                
+                var choicesObj = {
+                    // name: ((i+1) + " - [" + res[i].thingy_type + "] -- " + res[i].title) + " || Highest Bid: " + res[i].current_bid,
+                    name: myStr,
+                    
+                    value: res[i].id,
+                    short: res[i].thingy_type
+                }
                 // array of choice object
                 choicesArr.push(choicesObj);
             }
@@ -104,7 +121,7 @@ var options = {
                 var sel_id = answer['selection-id'];
 
                 // init another db query, retrieve logged bid
-                connection.query("SELECT * FROM all_stuff WHERE ?",
+                connection.query("SELECT * FROM auction_items WHERE ?",
                 [
                     {
                         id: sel_id
@@ -116,6 +133,8 @@ var options = {
                     console.log("\nYou've selected: " + res[0]['title']);
                     
                     current_bid = res[0]['current_bid'];
+                    current_bid = parseFloat(Math.round(current_bid * 100) / 100).toFixed(2);
+
                     console.log("Current highest bid: " + current_bid);
 
                     // inquirer amount to bid 
@@ -128,33 +147,32 @@ var options = {
                         }
                     ]).then(function(answer) {
                         var raw = answer['bid-attempt'];
-
+                        
                         // attempt formatting to x.00 notation
                         try {
                             var formatted = parseFloat(Math.round(raw * 100) / 100).toFixed(2);
-                            if (formatted != raw){
-                                console.log("Rounded to: " + formatted );
+                            if (!formatted === raw){
+                                console.log("\nRounded to: " + formatted );
                             }
                         }
                         catch (err){
                             console.log(err);
                         }
 
-                        // if (answer['bid-attempt'] > current_bid){
-                        if (formatted > current_bid) {
-                            console.log("You currently have the highest bid on this item!");
+                        if (parseFloat(formatted) > parseFloat(current_bid)) {
 
-                                connection.query(
-                                "UPDATE all_stuff SET ? WHERE ?",
-                                [
-                                {
-                                    current_bid: formatted
-                                },
-                                {
-                                    id: sel_id
-                                }
+                            console.log("\nYou currently have the highest bid on this item!");
+                                // connection.query('UPDATE users SET foo = ?, bar = ?, baz = ? WHERE id = ?', ['a', 'b', 'c', userId], function (error, results, fields) {
+                                // UPDATE auction_items SET current_bid=7.0, current_highest_bidder="marizu" WHERE id=4;
+
+                                connection.query('UPDATE auction_items SET ?, ? WHERE ?', 
+                                [ 
+                                    {current_bid : formatted}, 
+                                    {current_highest_bidder : user.username}, 
+                                    {id : sel_id} 
                                 ],
                                 function(err, res) {
+                                    if(err) throw (err);
                                     console.log(res.affectedRows + " item updated!\n");
                                     options.chooseOption();
                                 });
@@ -173,21 +191,8 @@ var options = {
         // display without rerunning options function
         options.dispList(false);
 
-        connection.query("SELECT * FROM all_stuff", function(err, res) {
+        connection.query("SELECT * FROM auction_items", function(err, res) {
             if (err) throw (err);
-            
-            // print all items
-            for (var i=0; i<res.length; i++){
-                var myStr = ( (i+1) + " - [" + res[i].thingy_type + "] -- " + res[i].title + " || Highest Bid: " + res[i].current_bid );
-                console.log(myStr + " \\\\");
-                
-                // dynamic line length shenanigans
-                var eql  = "=";
-                for (var y=0; y<myStr.length; y++){
-                    eql = eql + "=";
-                }
-                console.log(eql + "˩˩");
-            }
 
             inquirer
             .prompt([
@@ -203,7 +208,6 @@ var options = {
                 if (item_type === 'nevermind'){
                     // break;
                     console.log("\n\n\n\n\n");
-                    
                     options.chooseOption();
                 }
 
@@ -218,7 +222,7 @@ var options = {
                         message: ("What is the title of this " + item_type + "?"),
                     }
                 ]).then(function(answer) {
-                    var item_name = answer['item-name'];
+                    var item_name = answer['item-name'].trim();
                     
                     inquirer
                     .prompt([
@@ -228,7 +232,14 @@ var options = {
                             message: ("What would you like to start the bidding at?"),
                         }
                     ]).then(function(answer) {
-                        var starting_bid = answer['item-starting-bid'];
+                        var raw = answer['item-starting-bid'];
+
+                        try {
+                            var starting_bid = parseFloat(Math.round(raw * 100) / 100).toFixed(2);
+                        }
+                        catch (err) {
+                            console.log(err);
+                        }
 
                         console.log(
                             "\ntype or service: " + "[ " + item_type + " ]" + "\n" +
@@ -251,12 +262,16 @@ var options = {
                             else {
                                 try{
                                     connection.query(
-                                        "INSERT INTO all_stuff SET ?",
+                                        "INSERT INTO auction_items SET ?",
+                                        [
                                         {
-                                        thingy_type: item_type,
-                                        title: item_name,
-                                        current_bid: starting_bid,
-                                        },
+                                            thingy_type: item_type,
+                                            poster: user.username,
+                                            title: item_name,
+                                            current_bid: starting_bid,
+                                            current_highest_bidder: "None"
+                                        }
+                                        ],
                                         function(err, res) {
                                         console.log("Entry successfully added!\n");
                                         options.chooseOption();
@@ -280,15 +295,15 @@ var options = {
     },
 
     dispList : function (rerun){
-        connection.query("SELECT * FROM all_stuff", function(err, res) {
+        connection.query("SELECT * FROM auction_items", function(err, res) {
             if (err) throw err;
-            // console.log(res);
             
             console.log('\n');
-            console.log("[[ Great-Bay Auction List ]] ==========================\\\\");
+            console.log("[[ Great-Bay Auction List ]] ===========================================================\\\\");
 
-            for (var i=0; i<res.length; i++){
-                var myStr = ( (i+1) + " - [" + res[i].thingy_type + "] -- " + res[i].title + " || Highest Bid: " + res[i].current_bid );
+            for (var i = 0; i < res.length; i++){
+                var counter = i+1;
+                var myStr = ( counter + ") [" + res[i].thingy_type + "] [posted by: " + res[i].poster + "] -- " + res[i].title + " || Highest Bid: " + res[i].current_bid + " (" + res[i].current_highest_bidder + ")" );
                 console.log(myStr + " \\\\");
                 
                 // dynamic line length shenanigans
@@ -300,19 +315,203 @@ var options = {
             }
 
             console.log('\n\n\n');
+            console.log("( scroll up to see all. Arrow keys to see menu again )");
         });
         if (rerun){
             options.chooseOption();
         }
     },
+    quit : function (){
+        console.log("Ok, Goodbye!");
+        connection.end();
+        process.exit();
+    },
 };
 
+// Application Start
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
 
-    options.chooseOption();
+    home();
 });
+
+var user = {
+    username : '',
+    pass : '',
+    authenticated: false,
+    
+    login : function (){
+        inquirer
+        .prompt([
+            {
+                name: "uname",
+                type: "input",
+                message: ("Username:"),
+            }
+        ]).then(function(answer) {
+            user.username = answer.uname.trim();
+
+            inquirer
+            .prompt([
+                {
+                    name: "pass",
+                    type: "password",
+                    message: ("Password:"),
+                }
+            ]).then(function(answer) {
+                user.pass = answer.pass.trim();
+                
+                try {
+                    connection.query("SELECT * FROM users WHERE ?",
+                    [
+                    {
+                        uname: user.username,
+                    }
+                    ],
+                    function(err, res) {
+                        // console.log(res);
+                        // console.log(res[0].pass);
+                        if (user.username === res[0].uname && user.pass === res[0].pass){
+                            console.log("\nLogin successful ... \n");
+                            user.authenticated = true;
+                            options.chooseOption();
+                        }
+                        else{
+                            console.log("Login failed.\n")
+                            home();
+                        }
+
+                    });
+                }
+                catch (err){
+                    console.log(err);
+                }
+
+            });
+            
+        });
+    
+    },
+
+    signup : function (){
+        inquirer
+        .prompt([
+            {
+                name: "uname",
+                type: "input",
+                message: ("Register Username:"),
+            }
+        ]).then(function(answer) {
+            var attempt = {
+                uname : answer.uname.trim()
+            };
+
+            inquirer
+            .prompt([
+                {
+                    name: "pass",
+                    type: "password",
+                    message: ("Password:"),
+                }
+            ]).then(function(answer) {
+                attempt.pass = answer.pass.trim();
+
+                // console.log(attempt);
+
+                inquirer
+                .prompt([
+                    {
+                        name: "cpass",
+                        type: "password",
+                        message: ("Confirm Password:"),
+                    }
+                ]).then(function(answer) {
+                    if (attempt.pass === answer.cpass){
+                        // console.log("Passwords match");
+
+                        // check if username already exists
+                        connection.query("SELECT id FROM users WHERE ?",
+                        [
+                            {
+                                uname: attempt.uname   
+                            }
+                        ],
+                        function (err, res) {
+                            // console.log(res);
+                            
+                            // username not in db
+                            if (res.length === 0){
+                                console.log("\nAttemping to create account");
+
+                                connection.query("INSERT INTO users SET ?",
+                                [
+                                    {
+                                        uname: attempt.uname,
+                                        pass: attempt.pass
+                                    }
+                                ],
+                                function(err, res) {
+                                    console.log("account created successfully!\n");
+                                    home();
+                                });
+                            }
+                            else {
+                                console.log("That username is taken\n");
+                                home();
+                            }
+
+                        });
+                           
+                    }
+                    else{
+                        console.log("Your passwords must match!\n");
+                        home();
+                    }
+                });
+            });
+        });
+    },
+    
+    logout : function (){
+        this.username = '';
+        this.pass = '';
+        this.authenticated = false;
+
+        console.log("You've been logged out.\n");
+        home();
+    },
+}
+
+function home() {
+    inquirer
+    .prompt([
+        {
+            name: "sel",
+            type: "list",
+            message: ("Great-Bay Auctions"),
+            choices: ["Login", "Sign-up", "Exit"]
+        }
+    ]).then(function(answer) {
+
+        switch (answer.sel){
+            case "Login":
+                user.login();
+            break;
+        
+            case "Sign-up":
+                user.signup();
+            break;
+
+            case "Exit":
+                options.quit();
+            break;
+
+            default:
+            console.log("Some default stuff");
+        };
+    });
+}
 
 // // function createProduct() {
 // //     console.log("Inserting a new product...\n");
--- greatBaySeed.sql
+++ greatBaySeed.sql
@@ -2,24 +2,44 @@ DROP DATABASE IF EXISTS great_bay_db;
 CREATE DATABASE great_bay_db;
 USE great_bay_db;
 
-CREATE TABLE all_stuff (
+CREATE TABLE auction_items (
     id INT NOT NULL AUTO_INCREMENT,
-    thingy_type VARCHAR(45) NULL,               /* item, task, project, job */
-    title VARCHAR(45) NULL,
+	poster VARCHAR(32) NOT NULL,
+	thingy_type VARCHAR(32) NULL,               /* item, task, project, job */
+    title VARCHAR(64) NULL,
     current_bid DECIMAL(10,2) NULL,
+    current_highest_bidder VARCHAR(32) NULL,
 
     PRIMARY KEY (id)
 );
 
-INSERT INTO all_stuff (thingy_type, title, current_bid)
-VALUES ("item", "Moldy Slice of Pizza", 3.50),
-("item", "Jason's Laptop", 10.00),
-("job", "Hacking", 13.00),
-("task","Paint my room",  5.00),
-("project", "Destroy Jason's Laptop", 15.00);
+INSERT INTO auction_items (poster, thingy_type, title, current_bid, current_highest_bidder)
+VALUES ("marizu", "item", "Moldy Slice of Pizza", 3.50, "dancer"),
+("chlora", "item", "Jason's Laptop", 10.00, "tommyWiseau"),
+("RICHMAN", "job", "Hacking", 13.00, "fAntasTiCFISH_030"),
+("tommyWiseau", "task","Paint my room",  5.00, "None"),
+("chlora", "project", "Destroy Jason's Laptop", 15.00, "marizu"),
+("RICHMAN", "job", "Hit job", 15000.00, "None"),
+("RICHMAN", "item", "Michael's Pizzas", 75.00, "None");
 
-SELECT * FROM all_stuff WHERE ;
+SELECT * FROM auction_items;
 
+/* UPDATE auction_items SET current_bid=7.0, current_highest_bidder="marizu" WHERE id=4;
+SELECT id from users WHERE uname='fAntaTiCFISH_030' and pass='pass'; */
+
+-- Users Table
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT,
+    uname VARCHAR(32) NULL,
+    pass VARCHAR(32) NULL,
+
+    PRIMARY KEY (id)
+);
+
+INSERT INTO users (uname, pass)
+VALUES ("marizu", "pass"), ("chlora", "pass"), ("fAntaTiCFISH_030", "pass"), ("tommyWiseau", "tw"), ("dancer", "pass"), ("RICHMAN", "$$$");
+
+SELECT * FROM users;
 -- ### Alternative way to insert more than one row
 -- INSERT INTO products (flavor, price, quantity)
 -- VALUES ("vanilla", 2.50, 100), ("chocolate", 3.10, 120), ("strawberry", 3.25, 75);
